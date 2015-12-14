7ab49bd0c48f5342c35daa847365666223ca77d8
irmowan <irmowan@gmail.com>
2015-12-14 15:30:36 +0800

    Add naive version of emotion label
    By using dictionary of emotions and match them in weibo.

---
+++ weibo_label.py
@@ -0,0 +1,49 @@
+# Author: irmo
+# Date: 2015.12.14
+
+# For unicode reading
+import codecs
+
+# From emotions.txt generate a dictionary of emotions
+
+file_emotions = codecs.open('data/emotions.txt', 'r', 'utf-8')
+dic = {}
+for line in file_emotions:
+    x = line.split(':')
+    emotion = x[0][1:-1]
+    label = int(x[1])
+    dic[emotion] = label
+
+file_emotions.close()
+
+# Read line by line to
+file_tweets = codecs.open('data/tweets_sample.txt', 'r', 'utf-8')
+cnt = 0
+for line in file_tweets:
+    fields = line.split()
+    # Pick the weibo field up
+    weibo = ""
+    for i in range(5, len(fields)):
+        if fields[i] in ['m', 'f']:
+            break
+        weibo += fields[i]
+    pos = 0
+    nag = 0
+    for key in dic.keys():
+        if key in weibo:
+            if dic[key] == 1:
+                pos += 1
+            else:
+                nag += 1
+    print(weibo)
+    if pos > 0 and nag == 0:
+        print("Positive")
+    if nag > 0 and pos == 0:
+        print("Nagitive")
+    if pos > 0 and nag > 0:
+        print("Ambivalent, but more ", "positive" if pos > nag else "nagitive")
+    cnt += 1
+
+if __name__ == "__main__":
+    print("Count of weibo: ", cnt)
+    file_tweets.close()
