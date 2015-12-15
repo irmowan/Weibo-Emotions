22019105c2623b9a42f73a47c9e213b4f316d1b1
irmowan <irmowan@gmail.com>
2015-12-15 15:10:38 +0800

    Add emotion_forward

---
+++ applications/emotion_forward.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+
+from pyspark import SparkContext
+
+def predict(line):
+    fields = line.split()
+
+    tweet_id = fields[0]
+    text = fields[9]
+    npos = int(fields[-2])
+    nneg = int(fields[-1])
+
+    emotion = (npos and not nneg and "Positive") or (not npos and nneg and "Negative") or "Ambivalent"
+
+    return (emotion, 1 if '//' in text else 0)
+
+def analyse(path, outputPath):
+    lines = sc.textFile(sys.argv[1], use_unicode=False)
+
+    results = lines.map(predict).reduceByKey(add)
+
+    results.saveAsTextFile(outputPath)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: emotion_forward naive-data result–path")
+        exit(-1)
+
+    sc = SparkContext(appName = "emotion_forward")
+
+    analyse(sys.argv[1], sys.argv[2])
