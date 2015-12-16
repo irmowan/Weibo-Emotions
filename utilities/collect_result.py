d55b9239634e6e4dd348d9709c8040aff377294b
root <chenyuxin.mail@gmail.com>
2015-12-16 20:57:46 +0800

    save result locally

--- utilities/collect_result.py
+++ utilities/collect_result.py
@@ -7,11 +7,15 @@ from pyspark import SparkContext
 if __name__ == '__main__':
     sc = SparkContext(appName = "collect_result")
 
-    if len(sys.argv) != 3:
-        print("Usage(only running on local): collect_result input_path(hdfs) output_path(local)")
+    if len(sys.argv) < 3 or len(sys.argv) > 4:
+        print("Usage(only running on local): collect_result input_path(hdfs) output_path(local) #sample(optional)")
         exit(-1)
 
-    lines = sc.textFile(sys.argv[1]).collect()
+    if len(sys.argv) == 4:
+        nsample = int(sys.argv[3])
+        lines = sc.textFile(sys.argv[1]).takeSample(False, nsample)
+    else:
+        lines = sc.textFile(sys.argv[1]).collect()
 
     with codecs.open(sys.argv[2], 'w', "utf-8") as f:
         for line in lines:
